adc2b44d2bdb4d478955caf65f6ee4e99fcc5c8c
Adedotun Adepoju <adedotun.adepoju@b54.co>
2022-09-11 10:42:08 +0100

    Added configurations to the main file for creating infrastructures

--- terraform_gcp/terraform/main.tf
+++ terraform_gcp/terraform/main.tf
@@ -1,20 +1,46 @@
-  terraform {
+terraform {
+    required_version = ">=1.0"
+    backend "local" {}
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "3.5.0"
     }
   }
 }
 
 provider "google" {
-  credentials = file("<NAME>.json")
-
-  project = "<PROJECT_ID>"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  project = var.project
+  region  = var.region
+  //credentials = file("<NAME>.json") use this if you don't want to use the export env variable
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+# Data Lake Bucket
+resource "google_storage_bucket" "data-lake-bucket" {
+  name = "${local.data_lake_bucket}_${var.project}" # Concatenate DL bucket and project name
+  location = var.region
+
+  # Optional, but recommended settings
+  storage_class = var.storage_class
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 30 //days
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  force_destroy = true
+}
+
+resource "google_bigquery_dataset" "bq_dataset" {
+  dataset_id = var.BQ_DATASET
+  project = var.project
+  location = var.region
 }
