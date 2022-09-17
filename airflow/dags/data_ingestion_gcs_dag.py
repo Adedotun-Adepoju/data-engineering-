2b30e929ded34e80ffabc6830ee9cf9ed4e97ecc
Adedotun Adepoju <adedotun.adepoju@b54.co>
2022-09-17 20:36:01 +0100

    remove task to convert from csv to parquet

--- airflow/dags/data_ingestion_gcs_dag.py
+++ airflow/dags/data_ingestion_gcs_dag.py
@@ -12,16 +12,12 @@ from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExte
 import pyarrow.csv as pv
 import pyarrow.parquet as pq 
 
-import gdown
-
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
 BUCKET = os.environ.get("GCP_GCS_BUCKET")
 
-dataset_file = "yellow_tripdata_2021-01.csv"
-dataset_url = "https://drive.google.com/file/d/1PVxj7DIHZb3RIvuKTrhl873rBUcaonwd/view?usp=sharing"   
-file_id = dataset_url.split("/")[-2]
+dataset_file = "yellow_tripdata_2021-01.parquet"
+dataset_url = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2021-01.parquet"   
 path_to_local_home = os.environ.get("AIRFLOW_HOME", "opt/airflow/")
-parquet_file = dataset_file.replace('.csv', '.parquet')
 BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'trips_data_all')
 
 def download_from_drive(url, output):
@@ -74,30 +70,26 @@ with DAG(
     #     bash_command=f"gdown --id ${file_id} -O {dataset_file}"
     # )
 
-    download_dataset_task = PythonOperator(
+    download_dataset_task = BashOperator(
         task_id="download_Dataset_task",
-        python_callable = download_from_drive,
-        op_kwargs= {
-            'url':dataset_url,
-            'output':dataset_file
-        }
+        bash_command=f"curl {dataset_url} -o {path_to_local_home}/{dataset_file}"
     )
 
-    format_to_parquet = PythonOperator(
-        task_id="format_to_parquet_task",
-        python_callable=format_to_parquet,
-        op_kwargs={
-            "src_file": f"{path_to_local_home}/{dataset_file}"
-        }
-    )
+    # format_to_parquet = PythonOperator(
+    #     task_id="format_to_parquet_task",
+    #     python_callable=format_to_parquet,
+    #     op_kwargs={
+    #         "src_file": f"{path_to_local_home}/{dataset_file}"
+    #     }
+    # )
 
     local_to_gcs_task = PythonOperator(
         task_id="local_to_gcs_task",
         python_callable=upload_to_gcs,
         op_kwargs={
             "bucket": BUCKET,
-            "object_name": f"raw/{parquet_file}",
-            "local_file": f"{path_to_local_home}/{parquet_file}",
+            "object_name": f"raw/{dataset_file}",
+            "local_file": f"{path_to_local_home}/{dataset_file}",
         },
     )
 
@@ -111,9 +103,9 @@ with DAG(
             },
             "externalDataConfiguration": {
                 "sourceFormat": "PARQUET",
-                "sourceUris": [f"gs://{BUCKET}/raw/{parquet_file}"],
+                "sourceUris": [f"gs://{BUCKET}/raw/{dataset_file}"],
             },
         },
     )
 
-    download_dataset_task >> format_to_parquet >> local_to_gcs_task >> bigquery_external_table_task
+    download_dataset_task >> local_to_gcs_task >> bigquery_external_table_task
